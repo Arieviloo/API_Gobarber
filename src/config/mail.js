182ac121c2d610149bcebdd5768db940909286bf
Jadiê <jadieoliveira00@gmail.com>
2019-09-09 12:15:31 -0300

    cancelamento e config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b08d0dd4c6f9ee',
+    pass: '4a72761baf51b7',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import authMiddleware from './app/middleware/auth';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const router = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +20,9 @@ router.put('/users', UserController.update);
 router.get('/providers', ProviderController.index);
 router.post('/appointments', AppointmentController.store);
 router.get('/appointments', AppointmentController.index);
+router.delete('/appointments/:id', AppointmentController.delete);
 router.get('/schedule', ScheduleController.index);
+router.get('/notifications', NotificationController.index);
+router.put('/notifications/:id', NotificationController.update);
 router.post('/files', upload.single('file'), FileController.store);
 export default router;
